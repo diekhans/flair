a52c6414f836a8ebcde5cddd0e93278b28ec866b
Mark Diekhans <markd@ucsc.edu>
2026-02-03 20:43:44 -0800

    patch sort issue with fusion; really need to make it stable

--- src/flair/flair_fusion.py
+++ src/flair/flair_fusion.py
@@ -254,38 +254,22 @@ def detectfusions():
     bamtobedcmd = ('bedtools', 'bamtobed', '-bed12', '-i', args.output + '.syntheticAligned.bam')
     getsscommand = ['python3', path + 'synthetic_splice_sites.py', args.output + '.syntheticAligned.bed',
                         args.output + '-syntheticReferenceAnno.gtf', args.output + '.syntheticAligned.SJ.bed', args.output + '-syntheticBreakpointLoc.bed', '8', '2', args.output + '-syntheticFusionGenome.fa']#'15', '2']
-    use_transcriptome = True #False  # FIXME: tmp debugging
-    if use_transcriptome:
-        transcriptome_command = ['python3', path + 'flair_transcriptome.py',
-                                 '--genome_aligned_bam', args.output + '.syntheticAligned.bam',
-                                 '--genome', args.output + '-syntheticFusionGenome.fa',
-                                 '--gtf', args.output + '-syntheticReferenceAnno.gtf',
-                                 '--junction_bed', args.output + '.syntheticAligned.SJ.bed',
-                                 '--ss_window', '8',
-                                 '--generate_map',
-                                 '--quality', '0',
-                                 '--sjc_support', '2',
-                                 '--allow_paralogs',
-                                 '--end_window', '300',
-                                 '--no_check_splice',
-                                 '--no_stringent',
-                                 '--no_align_to_annot',
-                                 '--fusion_breakpoints', args.output + '-syntheticBreakpointLoc.bed',
-                                 '--output', args.output + '.syntheticAligned.flair',]
-                                #  '--keep_intermediate']
-    else:
-    ## NOT ADDING GTF ANNOT TO correct or collapse - I think this will save time down the line
-        correctcommand = ['python3', path + 'flair_correct.py', '-t', args.threads, '-q', args.output + '.syntheticAligned.bed',
-                          '-f', args.output + '-syntheticReferenceAnno.gtf',
-                          '--output', args.output + '.syntheticAligned.flair', '--junction_bed', args.output + '.syntheticAligned.SJ.bed', '--ss_window', '8']
-        collapsecommand = ['python3', path + 'flair_collapse.py', '-t', args.threads, '-q', args.output + '.syntheticAligned.flair_all_corrected.bed',
-                           '-g', args.output + '-syntheticFusionGenome.fa', #'-f', args.output + '-syntheticReferenceAnno.gtf',
-                           '--output', args.output + '.syntheticAligned.flair', '-r', freadsname, '--end_window', '300', #'--stringent', '--check_splice', #'--annotation_reliant', 'generate',
-                           '--generate_map', '--quality', '0', '--support', '2', '--fusion_breakpoints', args.output + '-syntheticBreakpointLoc.bed', '--allow_paralogs']#, '--keep_intermediate']
-
-
-    ##currently need to run correct and collapse as subprocess because they expect specific args, need to fix this at some point I think
-    # print(makesynthcommand)
+    transcriptome_command = ['python3', path + 'flair_transcriptome.py',
+                             '--genome_aligned_bam', args.output + '.syntheticAligned.bam',
+                             '--genome', args.output + '-syntheticFusionGenome.fa',
+                             '--gtf', args.output + '-syntheticReferenceAnno.gtf',
+                             '--junction_bed', args.output + '.syntheticAligned.SJ.bed',
+                             '--ss_window', '8',
+                             '--generate_map',
+                             '--quality', '0',
+                             '--sjc_support', '2',
+                             '--allow_paralogs',
+                             '--end_window', '300',
+                             '--no_check_splice',
+                             '--no_stringent',
+                             '--no_align_to_annot',
+                             '--fusion_breakpoints', args.output + '-syntheticBreakpointLoc.bed',
+                             '--output', args.output + '.syntheticAligned.flair',]
 
     pipettor.run([faidxcommand])
     print('synth genome made')
@@ -296,12 +280,7 @@ def detectfusions():
     pipettor.run([getsscommand])
     print('done getting ss')
 
-    if use_transcriptome:
-        pipettor.run(transcriptome_command)
-    else:
-        pipettor.run([correctcommand])
-        print('done with correct')
-        pipettor.run([collapsecommand])
+    pipettor.run(transcriptome_command)
 
     ##clean up isoform/gene names for args.output + '.combined.isoform.read.map.txt', '.isoforms.bed', '.isoforms.fa'
     oldnametonewname = {}

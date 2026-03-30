bd04c0d2a1070e19684230bb564468e0cb2d9b80
Colette Felton <cafelton@ucsc.edu>
2026-03-30 10:16:51 -0700

    add more fusion files, fix close genes on negative strand fusion filtering

---
+++ src/flair/filter_transcriptome_chim.py
@@ -0,0 +1,12 @@
+import sys
+import pysam
+
+infile = pysam.AlignmentFile(sys.argv[1], 'r') ##sam file input
+outname = sys.argv[2] ##bam suffix
+outfile = pysam.AlignmentFile(outname, 'wb', template=infile)
+for align in infile:
+    if align.is_mapped and not align.is_secondary:
+        if align.has_tag('SA'):
+            outfile.write(align)
+infile.close()
+outfile.close()
